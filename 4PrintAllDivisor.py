70c2e0cbad8bdfe0490d16b1da52d119792bbcfd
Mike Tran <trantrikien239@gmail.com>
2019-02-09 11:01:51 +0700

    Update 4PrintAllDivisor.py

--- 4PrintAllDivisor.py
+++ 4PrintAllDivisor.py
@@ -10,28 +10,20 @@ def getInt(text):
         print("Input must be a positive integer, please try again")
         return getInt(text)
 num = getInt("Give me a positive integer: ")
-print(1)
-# Find all divisor less than sqrt(num)
+
+# Find all divisor 
 import math
-l1 = range(1, math.floor(math.sqrt(num))+1)
 l2 = []
-print(2)
-for i in l1:
+
+for i in range(1, math.floor(math.sqrt(num))+1):
     if num % i == 0:
         l2.append(i)
-    print(3)
-print(l2)
-print(l1)
-# Find all divisor more than sqrt num
-l3 = []
-for i in l2:
-    l3.append(num/i)
-    l3.append(i)
-    print(4)
-
+        l2.append(num/i)
 # Remove duplicate
-if math.sqrt(num) == math.floor(math.sqrt(num)):
-    l3.remove(math.sqrt(num))
-    print(5)
+try:
+    l2.remove(math.sqrt(num))
+    print('')
+except(Exception):
+    print('')
 
-print(l3)
+print(l2)
