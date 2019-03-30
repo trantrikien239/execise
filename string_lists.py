8fdefdc6c7dd473365a6d8a4ca628b790b5b4b5a
Kien Tran <kientran@beeketing.com>
2019-03-30 16:12:22 +0700

    Add string_lists execise

---
+++ string_lists.py
@@ -0,0 +1,12 @@
+import math
+strings = input("Please provide a string: ")
+print(type(strings))
+ln = math.floor(len(strings)/2)
+test = True
+for i in range(ln):
+    test = test and (strings[i] == strings[-i])
+
+if test:
+    print("The string is a palindrome")
+else:
+    print("The string is not a palindrome")
