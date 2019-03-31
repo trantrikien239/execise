82554c2e635b029b0b3aa746eb441292f8401254
Kien Tran <kientran@beeketing.com>
2019-03-31 09:41:52 +0700

    Check primality of integer, using import from print all divisor

---
+++ check_primality.py
@@ -0,0 +1,7 @@
+import print_all_divisor as dvs
+print("Sorry, wrong request, I thought you were asking for divisors. Now, what integer do you want to check primality?")
+num = dvs.get_int("Give me a positive integer: ")
+if len(dvs.find_all_divisors(num)) == 2:
+    print("It's a prime number")
+else:
+    print("It's not a prime number")
