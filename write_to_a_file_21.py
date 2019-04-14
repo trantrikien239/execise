3dd627d7da563f4da4f83b9ae78e7d5495dceb59
Kien Tran <kientran@beeketing.com>
2019-04-15 00:00:58 +0700

    countries

--- write_to_a_file_21.py
+++ write_to_a_file_21.py
@@ -1,16 +1,15 @@
 import requests
-import lxml
 from bs4 import BeautifulSoup as bsp
 
 web_page_html = requests.get('https://www.worldstandards.eu/other/tlds/').text
-web_page = bsp(web_page_html,'html.parser')
+web_page = bsp(web_page_html,'lxml')
 
 
 # print(web_page.find_all(class_ = 'table table-striped'))
 table = web_page.find_all(class_ = 'table table-striped')[0]
 print(type(table))
 print(len(web_page.find_all(class_ = 'table table-striped')))
-list_data = table.findAll('td')
+list_data = table.find_all('td')
 with open('./data/country_domain.csv',mode='w+') as file:
     for index, item in enumerate(list_data):
         if index % 2 == 0:
@@ -18,6 +17,10 @@ with open('./data/country_domain.csv',mode='w+') as file:
         else:
             file.write(item.text + '\n')
 
-
-# for table in web_page.find_all(class_ = 'table table-striped'):
-#     table.tbody.
+web_page_eu_countries = bsp(requests.get('https://www.countries-ofthe-world.com/countries-of-europe.html').text,'lxml')
+data_area = web_page_eu_countries.find(class_ = 'container list-container')
+data_list = data_area.find_all('li')
+with open('./data/eu_countries.csv', mode = 'w+') as file_eu_countries:
+    for item in data_list:
+        if item.text not in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+            file_eu_countries.write(item.text + '\n')
